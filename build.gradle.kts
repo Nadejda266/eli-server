18396472ea028b11ef3a9bd30bf6460353fbd0ba
Nadejda266 <nadejdagusan51@gmail.com>
2026-05-21 16:03:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("org.mindrot:jbcrypt:0.4")
     implementation("ch.qos.logback:logback-classic:1.4.14")
+    // PostgreSQL driver
+    implementation("org.postgresql:postgresql:42.7.3")
 }
 
 tasks.shadowJar {
